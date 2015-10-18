99c945a26ec11ec99a92b54ee9af7b5067490489
aarose <contact@aarose.red>
2015-10-18 08:01:31 -0400

    Added more JS behaviour

--- minesweeper/static/site.js
+++ minesweeper/static/site.js
@@ -1,3 +1,28 @@
 $(function(){
-    // Script
+    var path = window.location.pathname;
+    console.log('CURRENT PATH: ' + path)
+
+    $("td").mousedown(function(event){
+        // Get the coordinates
+        var coords = $(this).attr('id')
+        var results = coords.split("-")
+        var x = results[1]
+        var y = results[2]
+        console.log('X: ' + x + ' | Y: ' + y)
+        switch (event.which) {
+            case 1:  // Left mouse button
+                console.log('CLICKED ' + coords)
+                // Ajax request to current url + /cell/x,y, POST 
+                $(this).load("", function(responseTxt, statusTxt, xhr){
+                    if(statusTxt == "success")
+                        alert("External content loaded successfully!");
+                    if(statusTxt == "error")
+                        alert("Error: " + xhr.status + ": " + xhr.statusText);
+                });
+                break;
+            case 3: // Right mouse button
+                console.log('RIGHT MOUSE CLICK ' + coords)
+                break;
+        }
+    });
 });
